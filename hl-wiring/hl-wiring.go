8689757afcef5ce2793a9a574f07c122cd64013a
Mirko Maischberger <mirko.maischberger@gmail.com>
2016-10-27 21:05:11 +0200

    Fix in wiring component

--- hl-wiring/hl-wiring.go
+++ hl-wiring/hl-wiring.go
@@ -15,21 +15,26 @@ import (
 	"github.com/eclipse/paho.mqtt.golang"
 )
 
-type rulePayload struct {
-	payload string
+// Config TOML main configuration
+// [rule."in/topic"."out/topic"]
+// payload=...
+//
+type Config struct {
+	Rules map[string]map[string]RulePayload `toml:"rule"`
 }
 
-type config struct {
-	rules map[string]map[string]rulePayload `toml:"rule"`
+// RulePayload TOML rule payload to be sent
+type RulePayload struct {
+	Payload string `toml:"payload"`
 }
 
-func republish(channel chan mqtt.Message, queue mqtt.Client, cfg config) {
+func republish(channel chan mqtt.Message, queue mqtt.Client, config Config) {
 	for {
 		msg := <-channel
 		topic := msg.Topic()
-		for k, v := range cfg.rules[topic] {
+		for k, v := range config.Rules[topic] {
 			log.Println("Received:", topic, "Sending:", k, v)
-			if token := queue.Publish(k, 0, false, v.payload); token.Wait() && token.Error() != nil {
+			if token := queue.Publish(k, 0, false, v.Payload); token.Wait() && token.Error() != nil {
 				panic(token.Error())
 			}
 		}
@@ -43,16 +48,16 @@ func main() {
 	mqttServer := flag.String("mqtt", "tcp://localhost:1883", "MQTT address")
 	flag.Parse()
 
-	var cfg config
-
-	if _, err := toml.DecodeFile(os.ExpandEnv("${HOME}/.homely/wiring.toml"), &cfg); err != nil {
+	var config Config
+	configFile := os.ExpandEnv("${HOME}/.homely/wiring.toml")
+	if _, err := toml.DecodeFile(configFile, &config); err != nil {
 		panic(err)
 	}
 
-	log.Println(cfg)
+	log.Println("Loaded configuration: ", configFile, config)
 
 	topics := make(map[string]byte)
-	for k := range cfg.rules {
+	for k := range config.Rules {
 		topic := k
 		log.Println("Subscribe:", topic)
 		topics[topic] = 0
@@ -60,7 +65,7 @@ func main() {
 
 	channel := make(chan mqtt.Message)
 	queue := mqtt.NewClient(homely.MakeMqttPublishOptions(os.ExpandEnv("hl-wiring-${HOSTNAME}"), mqttServer, channel))
-	go republish(channel, queue, cfg)
+	go republish(channel, queue, config)
 	homely.MqttConnectAndSubscribe(queue, topics)
 
 	homely.MainLoop()
